564de38657f3f233c465bbcec4e1002e90ef0815
Bart Willems <ps3bartwillems@gmail.com>
2016-11-20 16:33:40 +0100

    Fix register, added broken .htaccess

--- php_functions/register.php
+++ php_functions/register.php
@@ -1,48 +1,72 @@
 <?php
 if(isset($_POST['submit'])){
-	include('connection.php');
-	include('sanitizer.php');
-	session_start();
-	$_SESSION['error'] = '';
+    include('connection.php');
+    include('sanitizer.php');
+    session_start();
+    $_SESSION['error'] = '';
 
-	if(isset($_POST['username'])) {
-		$username = sanitize(strtolower($_POST['username']));
-	} else {
-	    $_SESSION['error'] = "username is empty";
-	}
-	
-	if(isset($_POST['password'])) {
-		$password = $_POST['password'];
-	} else {
-	    $_SESSION['error'] = "password is empty";
-	}
-	
-	if(isset($_POST['password_match'])) {
-		$password_match = $_POST['password_match'];
-	} else {
-	    $_SESSION['error'] = "passwords don't match";
-	}
-	
-	$stmt = $mysqli->prepare("SELECT username FROM users WHERE username=? LIMIT 1");
-	
-	$stmt->bind_param('s', $username);
-	$stmt->execute($stmt);
-	
-    $count = 0;
-    while($stmt->fetch()){
-        $count++;
+    if(isset($_POST['username'])) {
+        $username = sanitize(strtolower($_POST['username']));
+    } else {
+        $_SESSION['error'] = "username is empty";
     }
+
+    if(isset($_POST['password'])) {
+        $password = $_POST['password'];
+    } else {
+        $_SESSION['error'] = "password is empty";
+    }
+
+    if(isset($_POST['password_match'])) {
+        $password_match = $_POST['password_match'];
+    } else {
+        $_SESSION['error'] = "passwords don't match";
+    }
+
+    if(!$mysqli){
+        $_SESSION['error'] = 'Database error';
+        if (isset($_SESSION['page'])){
+            $page = str_replace('"', "", $_SESSION['page']);
+            header("location: $page");
+        } else {
+            header("location: index.php");
+        }
+    }
+
+    $stmt = $mysqli->prepare("SELECT username FROM users WHERE username = ? LIMIT 1");
+
+    $stmt->bind_param('s', $username);
+    if(!$stmt->execute()){
+        $stmt->close();
+        $mysqli->close();
+        $_SESSION['error'] = 'Unexpected database error';
+        if (isset($_SESSION['page'])){
+            $page = str_replace('"', "", $_SESSION['page']);
+            header("location: $page");
+        } else {
+            header("location: index.php");
+        }
+    } else {
+        $count = 0;
+        while($stmt->fetch()){
+            $count++;
+        }
+    }
+
     if($count>0){
         $_SESSION['error'] = 'Error, username is not available.';
     } else {
-	    if(empty($_SESSION['error'])) {
-	        // Saul Goodman
+        if(empty($_SESSION['error'])) {
+            // Saul Goodman
             // password_hash with  the parameter PASSWORD_DEFAULT generates a 60character bcrypt string with the salt included
-	        $password_hash = password_hash("$password", PASSWORD_DEFAULT);
+            $password_hash = password_hash("$password", PASSWORD_DEFAULT);
             $stmt = $mysqli->prepare("INSERT INTO users(username,password) VALUES (?,?)");
             $stmt->bind_param("ss",$username,$password_hash);
-            $stmt->execute();
-            $_SESSION['result'] = 'Successfully registered! Please wait for your administrator for approval.';
+            if($stmt->execute()){
+                $_SESSION['result'] = 'Successfully registered! Please wait for your administrator for approval.';
+            } else {
+                $_SESSION['error'] = 'Unexpected error while registering.';
+            }
         }
     }
     $stmt->close();
